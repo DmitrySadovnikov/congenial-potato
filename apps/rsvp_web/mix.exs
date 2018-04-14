27c74e1bfed6925efa419f65c460e9bbc299804e
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2018-04-14 19:44:58 +0300

    add create page

--- apps/rsvp_web/mix.exs
+++ apps/rsvp_web/mix.exs
@@ -37,6 +37,7 @@ defmodule RsvpWeb.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
+     {:phoenix_ecto, "~> 3.1.0-rc.0"},
      {:cowboy, "~> 1.0"}]
   end
 end
